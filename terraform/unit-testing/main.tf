d73b52ff17d06772e9039a214e5f5bc2089676cc
Suyog Chinche <suyogchinche@Suyogs-MacBook-Air.local>
2024-05-16 21:59:14 +0530

    updated git ignore file

---
+++ terraform/unit-testing/main.tf
@@ -0,0 +1,18 @@
+# main.tf
+
+provider "aws" {
+    region = "eu-central-1"
+}
+
+variable "bucket_prefix" {
+  type = string
+  default = "test"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${var.bucket_prefix}-bucket"
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.bucket.bucket
+}
