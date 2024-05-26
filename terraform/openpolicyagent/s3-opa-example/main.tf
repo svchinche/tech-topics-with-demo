bd25b2790721337d7b4427f189b5802715cc1637
Suyog Chinche <suyogchinche@Suyogs-MacBook-Air.local>
2024-05-26 11:41:23 +0530

    added test

--- terraform/openpolicyagent/s3-opa-example/main.tf
+++ terraform/openpolicyagent/s3-opa-example/main.tf
@@ -1,17 +1,17 @@
 resource "aws_s3_bucket" "s3-bucket" {
   bucket = "my-s3-bucket"
-  acl = "private"
+  acl = "public-read"
 #  tags = {
 #    Name    = "s3-bucket"
 #    Product = "s3-product"
 #  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
+#  server_side_encryption_configuration {
+#    rule {
+#      apply_server_side_encryption_by_default {
+#        sse_algorithm = "AES256"
+#      }
+#    }
+#  }
   versioning {
     enabled = false
   }
@@ -19,10 +19,12 @@ resource "aws_s3_bucket" "s3-bucket" {
     enabled = true
     abort_incomplete_multipart_upload_days = 1
   }
-  public_access_block {
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
-  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_public" {
+  bucket = aws_s3_bucket.s3-bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
